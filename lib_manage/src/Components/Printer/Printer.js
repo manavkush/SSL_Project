08a2a0d981c8b27bd92b04b01589d2efe5059479
Pratik <190010034@iitdh.ac.in>
2020-12-06 00:15:59 +0530

    Pushing changes in printer forms

--- lib_manage/src/Components/Printer/Printer.js
+++ lib_manage/src/Components/Printer/Printer.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./Printer.css";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+
 function Printer(props) {
 
 	const printingSpeci_ph = "Pages 1 - 4 EXCEPT Page 3 (Custom Text) ";
@@ -11,61 +12,77 @@ function Printer(props) {
 		<div className="container">
 			<h1 className="header">Hassle free Printing</h1>
 
-			<form action="/printQuery" className="formClass" method="POST">
-				<div className="form-group">
-					<label for="uploadingFiles">Upload Files to Print</label>
+			<form action="/printmg/uploadDoc" className="formClass" method="POST" encType = "multipart/form-data">
+				//-----------File----------------------------------------
+				{/* <div className="form-group">
+					<label for="uploadingFiles " >Upload Files to Print</label>
 					<br />
-					<input type="file" multiple />
-				</div>
+					<input type="file" name="file" id = "file"/>
+				</div> */}
 
+				//-----------Color-------------------------------------
 				<div className="form-group">
 					<label for="color">Color</label>
-					<select className="form-control" style={{ width: "50%" }} id="color">
+					<select name = "color" className="form-control" style={{ width: "50%" }} id="color">
 						<option value="b/w">Black and White</option>
 						<option value="color">Color</option>
 					</select>
 				</div>
+				//---------------Size---------------------------------
 
 				<div className="form-group">
 					<label for="paperSize">Paper Size</label>
-					<select className="form-control" style={{ width: "50%" }} id="paperSize">
+					<select name="size" className="form-control" style={{ width: "50%" }} id="paperSize">
 						<option value="A3">A3</option>
 						<option value="A4">A4</option>
 						<option value="A5">A5</option>
 					</select>
 				</div>
 
+				//---------------Copies-------------------------------------
 				<div className="form-group">
 					<label for="copies">Copies</label>
-					<input className="form-control" type="number" style={{ width: "50%" }} placeholder="Number of Copies"></input>
+					<input name="copies" className="form-control" type="number" style={{ width: "50%" }} placeholder="Number of Copies"></input>
 				</div>
-
+				
+				//------------------Both Sides-------------------------------
 				<div className="form-group">
 					<label for="bothSides">Print on Both Sides</label>
-					<select className="form-control" style={{ width: "50%" }} id="paperSize">
+					<select name="bothSides" className="form-control" style={{ width: "50%" }} id="paperSize">
 						<option value="Yes">Yes</option>
 						<option value="No">No</option>
 					</select>
 				</div>
+				//------------------Printing Details-------------------------------
 
 				<div class="form-group">
 					<label for="printingDetails">Printing Details</label>
-					<textarea class="form-control" rows="3" style={{ width: "50%" }} placeholder={printingSpeci_ph}></textarea>
+					<textarea name="printingDetails" class="form-control" rows="3" style={{ width: "50%" }} placeholder={printingSpeci_ph}></textarea>
 				</div>
 
+				//------------------Gen Instructions-------------------------------
 				<div class="form-group">
 					<label for="genInstruc">General Instructions</label>
-					<textarea class="form-control" rows="3" style={{ width: "50%" }} placeholder={genInstruc_ph}></textarea>
+					<textarea name="genInstruc" class="form-control" rows="3" style={{ width: "50%" }} placeholder={genInstruc_ph}></textarea>
 				</div>
 
+				//---------------Submit----------------------------
 				<div className="form-group">
-					<button className="btn btn-primary" type="submit" value="Submit" >
-						Print
-					</button>
+					<button className="btn btn-primary" type="submit" value="Submit" >Submit</button>
 				</div>
 			</form>
 		</div>
 	);
 }
 
+// function Printer(props){
+
+// 	return(
+// 		<div>
+// 			<form action="/testing" method="POST">
+// 				<input type="submit" value="Submit">Submit</input>
+// 			</form>
+// 		</div>
+// 	);
+// }
 export default Printer;
